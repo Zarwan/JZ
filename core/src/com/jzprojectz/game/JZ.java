8b4c9799986bc807b5b0099dd711e7a4e609ffb3
Joanne Hong <joanne.hong@shopify.com>
2016-04-10 15:32:01 -0400

    integrate GestureDetector interface

--- core/src/com/jzprojectz/game/JZ.java
+++ core/src/com/jzprojectz/game/JZ.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.objects.TextureMapObject;
 import com.badlogic.gdx.maps.tiled.TideMapLoader;
@@ -23,9 +24,10 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
-public class JZ extends ApplicationAdapter {
+public class JZ extends ApplicationAdapter implements GestureDetector.GestureListener {
     private static final int ARROW_WIDTH = 200;
     private static final int ARROW_HEIGHT = 151;
     private TiledMap tiledMap;
@@ -65,6 +67,9 @@ public class JZ extends ApplicationAdapter {
         wrightBounds = new Rectangle(w - ARROW_WIDTH, 0, ARROW_WIDTH, ARROW_HEIGHT);
         leftTexture = new Texture(Gdx.files.internal("left_arrow.png"));
         rightTexture = new Texture(Gdx.files.internal("right_arrow.png"));
+
+        GestureDetector gestureDetector = new GestureDetector(this);
+        Gdx.input.setInputProcessor(gestureDetector);
     }
 
     @Override
@@ -76,22 +81,58 @@ public class JZ extends ApplicationAdapter {
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
 
-        for (int i = 0; i < 5; i++) {
-            if (!Gdx.input.isTouched(i)) continue;
-
-            guicam.unproject(touchPoint.set(Gdx.input.getX(i), Gdx.input.getY(i), 0));
-
-            if (wleftBounds.contains(touchPoint.x, touchPoint.y)) {
-                System.out.println("Left!");
-            } else if (wrightBounds.contains(touchPoint.x, touchPoint.y)) {
-                System.out.println("Right!");
-            }
-        }
-
         spriteBatch.begin();
         spriteBatch.draw(texture, 0, 0);
         spriteBatch.draw(leftTexture, wleftBounds.x, wleftBounds.y, wleftBounds.width, wleftBounds.height);
         spriteBatch.draw(rightTexture, wrightBounds.x, wrightBounds.y, wrightBounds.width, wrightBounds.height);
         spriteBatch.end();
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        guicam.unproject(touchPoint.set(x, y, 0));
+        if (wleftBounds.contains(touchPoint.x, touchPoint.y)) {
+            camera.position.x -= 300;
+            camera.update();
+        } else if ( wrightBounds.contains(touchPoint.x, touchPoint.y) ) {
+            camera.position.x += 300;
+            camera.update();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
     }
 }
