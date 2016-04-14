991c062f010e673838d49ef3de89424ade4f5694
Joanne Hong <joanne.hong@shopify.com>
2016-04-13 21:23:44 -0400

    add continuous movement logic

--- core/src/com/jzprojectz/game/JZ.java
+++ core/src/com/jzprojectz/game/JZ.java
@@ -2,19 +2,18 @@ package com.jzprojectz.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
-public class JZ extends ApplicationAdapter implements GestureDetector.GestureListener {
+public class JZ extends ApplicationAdapter implements InputProcessor {
     public static final float UNIT = 32;
     private static final float X_ARROW_WIDTH = 150/UNIT;
     private static final float X_ARROW_HEIGHT = 113/UNIT;
@@ -22,6 +21,11 @@ public class JZ extends ApplicationAdapter implements GestureDetector.GestureLis
     private static final float Y_ARROW_HEIGHT = 150/UNIT;
     private static final float SCREEN_WIDTH = 30;
     private static final float SCREEN_HEIGHT = 20;
+    private static final int NEUTRAL = 0;
+    private static final int LEFT = 1;
+    private static final int RIGHT = 2;
+    private static final int UP = 3;
+    private static final int DOWN = 4;
     private TiledMap tiledMap;
     private OrthographicCamera camera;
     private TiledMapRenderer tiledMapRenderer;
@@ -35,6 +39,7 @@ public class JZ extends ApplicationAdapter implements GestureDetector.GestureLis
     private Player player;
     private int mapWidth;
     private int mapHeight;
+    private int direction = NEUTRAL;
 
     @Override
     public void create() {
@@ -59,15 +64,12 @@ public class JZ extends ApplicationAdapter implements GestureDetector.GestureLis
         guicam.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
         guicam.update();
 
-
         leftArrowKey = new Button(SCREEN_WIDTH - X_ARROW_WIDTH*2, Y_ARROW_HEIGHT, X_ARROW_WIDTH, X_ARROW_HEIGHT, "left_arrow.png");
         rightArrowKey = new Button(SCREEN_WIDTH - X_ARROW_WIDTH, Y_ARROW_HEIGHT, X_ARROW_WIDTH, X_ARROW_HEIGHT, "right_arrow.png");
         upArrowKey = new Button(SCREEN_WIDTH - X_ARROW_WIDTH - Y_ARROW_WIDTH/2, Y_ARROW_HEIGHT + X_ARROW_HEIGHT, Y_ARROW_WIDTH, Y_ARROW_HEIGHT, "up_arrow.png");
         downArrowKey = new Button(SCREEN_WIDTH - X_ARROW_WIDTH - Y_ARROW_WIDTH/2, 0, Y_ARROW_WIDTH, Y_ARROW_HEIGHT, "down_arrow.png");
 
-
-        GestureDetector gestureDetector = new GestureDetector(this);
-        Gdx.input.setInputProcessor(gestureDetector);
+        Gdx.input.setInputProcessor(this);
     }
 
     @Override
@@ -88,32 +90,79 @@ public class JZ extends ApplicationAdapter implements GestureDetector.GestureLis
         spriteBatch.draw(upArrowKey.getTexture(), upArrowKey.getXBound(), upArrowKey.getYBound(), upArrowKey.getWidth(), upArrowKey.getHeight());
         spriteBatch.draw(downArrowKey.getTexture(), downArrowKey.getXBound(), downArrowKey.getYBound(), downArrowKey.getWidth(), downArrowKey.getHeight());
         spriteBatch.end();
+
+        switch (direction) {
+            case NEUTRAL:
+                break;
+            case LEFT:
+                camera.position.x -= 1;
+                player.moveLeft();
+                camera.update();
+                break;
+            case RIGHT:
+                camera.position.x += 1;
+                player.moveRight();
+                camera.update();
+                break;
+            case UP:
+                camera.position.y += 1;
+                player.moveUp();
+                camera.update();
+                break;
+            case DOWN:
+                camera.position.y -= 1;
+                player.moveDown();
+                camera.update();
+                break;
+        }
     }
 
     @Override
-    public boolean touchDown(float x, float y, int pointer, int button) {
-        guicam.unproject(touchPoint.set(x, y, 0));
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        guicam.unproject(touchPoint.set(screenX, screenY, 0));
 
         if (leftArrowKey.clicked(touchPoint.x, touchPoint.y) && notOnLeftBorder()) {
-            player.moveLeft();
-            camera.position.x -= 1;
-            camera.update();
+            direction = LEFT;
         } else if (rightArrowKey.clicked(touchPoint.x, touchPoint.y) && notOnRightBorder()) {
-            player.moveRight();
-            camera.position.x += 1;
-            camera.update();
+            direction = RIGHT;
         } else if (upArrowKey.clicked(touchPoint.x, touchPoint.y) && notOnTopBorder()) {
-            player.moveUp();
-            camera.position.y += 1;
-            camera.update();
+            direction = UP;
         } else if (downArrowKey.clicked(touchPoint.x, touchPoint.y) && notOnBottomBorder()) {
-            player.moveDown();
-            camera.position.y -= 1;
-            camera.update();
+            direction = DOWN;
         }
         return false;
     }
 
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        guicam.unproject(touchPoint.set(screenX, screenY, 0));
+
+        if (leftArrowKey.clicked(touchPoint.x, touchPoint.y)) {
+            direction = NEUTRAL;
+        } else if (rightArrowKey.clicked(touchPoint.x, touchPoint.y)) {
+            direction = NEUTRAL;
+        } else if (upArrowKey.clicked(touchPoint.x, touchPoint.y)) {
+            direction = NEUTRAL;
+        } else if (downArrowKey.clicked(touchPoint.x, touchPoint.y)) {
+            direction = NEUTRAL;
+        }
+        return false;
+    }
 
     private boolean notOnLeftBorder() {
         return (camera.position.x - SCREEN_WIDTH/2) > 0;
@@ -132,37 +181,17 @@ public class JZ extends ApplicationAdapter implements GestureDetector.GestureLis
     }
 
     @Override
-    public boolean tap(float x, float y, int count, int button) {
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
 
     @Override
-    public boolean longPress(float x, float y) {
+    public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
     @Override
-    public boolean fling(float velocityX, float velocityY, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean pan(float x, float y, float deltaX, float deltaY) {
-        return false;
-    }
-
-    @Override
-    public boolean panStop(float x, float y, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean zoom(float initialDistance, float distance) {
-        return false;
-    }
-
-    @Override
-    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+    public boolean scrolled(int amount) {
         return false;
     }
 }
